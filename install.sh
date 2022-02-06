d95cdc97a048cee728fd7d2c249b37649dc8a0cb
Kayla Altepeter <kayla.altepeter@datasite.com>
2022-02-06 10:50:35 -0600

    feat(passlib): adding

--- install.sh
+++ install.sh
@@ -46,6 +46,8 @@ EOD
     # recommended ansible install for mac: https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html#from-pip3
     python3 -m pip install --user ansible | tee -a "${log_file}"
   fi
+
+  python3 -m pip install --user passlib | tee -a "${log_file}"
   
   "$(python3 -m site --user-base)"/bin/ansible-pull --url https://github.com/DatasiteLabs/ds-labs-local-setup -i hosts --ask-become-pass
 
