d460f88931d1811b8b2e532b9374efbd9fb84a22
Kayla Altepeter <kayla.altepeter@datasite.com>
2022-02-14 10:02:29 -0600

    feat(install): simplify install

--- install.sh
+++ install.sh
@@ -16,48 +16,69 @@ fi
 echo ""
 read -r -p "Running in ${__dir}, this will be your DATASITE_HOME. Press [enter] to continue."
 echo ""
-read -r -p "enable script editor and terminal: https://developer.apple.com/library/archive/documentation/LanguagesUtilities/Conceptual/MacAutomationScriptingGuide/AutomatetheUserInterface.html. press [enter] to continue after complete"
+
+# install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo ""
-# Close any open System Preferences panes, to prevent them from overriding
-# settings we’re about to change
-osascript -e 'tell application "System Preferences" to quit'
+brew doctor | tee -a "${log_file}"
+echo ""
+read -r -p "Check the output of the 'brew doctor' command above. Fix any issues and re-run 'brew doctor'. Press [enter] to continue if there were no issues."
+echo ""
 
-# run a script to allow terminal to control apps
-sleep 2
-osascript <<EOD
-  tell application "System Events"
-      activate
-      display dialog "This should allow scripts to execute after you allow access."
-  end tell
-EOD
+brew install ansible
 
-echo "Follow the prompts to install xcode command line tools."
+# validate installs
+if ! xcode-select -p; then
+  echo "xcode-select tools did not complete."
+  exit 1
+else 
+  echo "xcode-select tools installed."
+fi
 
-if [[ $(uname -s) == "Darwin" ]]; then
-  if ! xcode-select -v; then
-    sleep 2
-    xcode-select --install
-    read -r -p "Wait for the xcode installer to complete. Press [enter] to continue."
-    if ! xcode-select -v; then
-      echo "xcode-select tools did not complete."
-      exit 1
-    fi
-  fi
+python --version
 
-  # ensure pip is available
-  python3 -m ensurepip --default-pip
+# read -r -p "enable script editor and terminal: https://developer.apple.com/library/archive/documentation/LanguagesUtilities/Conceptual/MacAutomationScriptingGuide/AutomatetheUserInterface.html. press [enter] to continue after complete"
+# echo ""
+# # Close any open System Preferences panes, to prevent them from overriding
+# # settings we’re about to change
+# osascript -e 'tell application "System Preferences" to quit'
 
-  # upgrade pip3
-  python3 -m pip install --upgrade pip setuptools wheel
+# # run a script to allow terminal to control apps
+# sleep 2
+# osascript <<EOD
+#   tell application "System Events"
+#       activate
+#       display dialog "This should allow scripts to execute after you allow access."
+#   end tell
+# EOD
 
-  # recommended ansible install for mac: https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html#from-pip3
-  python3 -m pip install --user ansible | tee -a "${log_file}"
+# echo "Follow the prompts to install xcode command line tools."
 
-  # # deps for ansible
-  python3 -m pip install --user passlib | tee -a "${log_file}"
-  python3 -m pip install --user pexpect | tee -a "${log_file}"
+# if [[ $(uname -s) == "Darwin" ]]; then
+#   if ! xcode-select -v; then
+#     sleep 2
+#     xcode-select --install
+#     read -r -p "Wait for the xcode installer to complete. Press [enter] to continue."
+#     if ! xcode-select -v; then
+#       echo "xcode-select tools did not complete."
+#       exit 1
+#     fi
+#   fi
 
-  DATASITE_HOME=${__dir} "$(python3 -m site --user-base)"/bin/ansible-pull --url https://github.com/DatasiteLabs/ds-labs-local-setup -i hosts
+#   # ensure pip is available
+#   python3 -m ensurepip --default-pip
+
+#   # upgrade pip3
+#   python3 -m pip install --upgrade pip setuptools wheel
+
+#   # recommended ansible install for mac: https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html#from-pip3
+#   python3 -m pip install --user ansible | tee -a "${log_file}"
+
+#   # # deps for ansible
+#   python3 -m pip install --user passlib | tee -a "${log_file}"
+#   python3 -m pip install --user pexpect | tee -a "${log_file}"
+
+#   DATASITE_HOME=${__dir} "$(python3 -m site --user-base)"/bin/ansible-pull --url https://github.com/DatasiteLabs/ds-labs-local-setup -i hosts
 
   exec -l "$SHELL"
 
