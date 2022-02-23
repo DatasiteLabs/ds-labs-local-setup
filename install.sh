c0c20b4431fa1e87d5e7f58452a5fa750df96bfb
Kayla Altepeter <kayla.altepeter@datasite.com>
2022-02-22 22:58:34 -0600

    fix(install): ansible

--- install.sh
+++ install.sh
@@ -94,7 +94,8 @@ fi
 #   python3 -m pip install --user passlib | tee -a "${log_file}"
 #   python3 -m pip install --user pexpect | tee -a "${log_file}"
 
-DATASITE_HOME=${__dir} ansible-pull --url https://github.com/DatasiteLabs/ds-labs-local-setup --connection=local --inventory 127.0.0.1, -v
+export DATASITE_HOME=${__dir} 
+ansible-pull --url https://github.com/DatasiteLabs/ds-labs-local-setup --connection=local --inventory 127.0.0.1, --limit 127.0.0.1 -i hosts -vvv
 
 exec -l "$SHELL"
 
