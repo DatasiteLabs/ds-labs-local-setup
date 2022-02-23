3b9993102734a7f4c9411bb590058cce50d7c05d
Kayla Altepeter <kayla.altepeter@datasite.com>
2022-02-22 22:44:18 -0600

    fix(install): path

--- install.sh
+++ install.sh
@@ -94,7 +94,7 @@ fi
 #   python3 -m pip install --user passlib | tee -a "${log_file}"
 #   python3 -m pip install --user pexpect | tee -a "${log_file}"
 
-DATASITE_HOME=${__dir} ansible-pull --url https://github.com/DatasiteLabs/ds-labs-local-setup -i hosts -vv
+DATASITE_HOME=${__dir} ansible-pull --url https://github.com/DatasiteLabs/ds-labs-local-setup -v
 
 exec -l "$SHELL"
 
